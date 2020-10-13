c6d3e6687c7aad24a05d9c92c90a74b5eb2807f3
Ben Toews <mastahyeti@gmail.com>
2020-10-12 20:20:38 -0400

    more data in expandable field

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-
 const earthRadius = 3958.8; // miles
-
-// https://www.movable-type.co.uk/scripts/latlong.html
 
 // miles from site to user.
 function milesFromSite(user, site) {
@@ -31,6 +28,22 @@ function bearingFromSite(user, site) {
 
     const y = Math.sin(userLam - siteLam) * Math.cos(userPhi);
     const x = Math.cos(sitePhi) * Math.sin(userPhi) - Math.sin(sitePhi) * Math.cos(userPhi) * Math.cos(userLam-siteLam);
+    const theta = Math.atan2(y, x);
+    const bearing = (theta * 180/Math.PI + 360) % 360;
+
+    return Math.round(bearing);
+}
+
+// azimuth from user to site.
+function bearingToSite(user, site) {
+    const sitePhi = site.siteLat * Math.PI/180;
+    const siteLam = site.siteLon * Math.PI/180;
+
+    const userPhi = user.latitude * Math.PI/180;
+    const userLam = user.longitude * Math.PI/180;
+
+    const y = Math.sin(siteLam - userLam) * Math.cos(sitePhi);
+    const x = Math.cos(userPhi) * Math.sin(sitePhi) - Math.sin(userPhi) * Math.cos(sitePhi) * Math.cos(siteLam-userLam);
     const theta = Math.atan2(y, x);
     const bearing = (theta * 180/Math.PI + 360) % 360;
 
@@ -70,12 +83,27 @@ fetch('sites.json').then(function(response) {
             sites.forEach(function(site) {
                 site.li = siteTemplate.cloneNode(true);
                 site.li.querySelector('div.frequency').innerText = site.frequency.toString() + ' MHz';
+                site.li.querySelector('div.city').innerText = site.city;
+                site.li.querySelector('div.state').innerText = site.state;
+                site.li.querySelector('div.latitude').innerText = site.siteLat.toString() + '\u00b0';
+                site.li.querySelector('div.longitude').innerText = site.siteLon.toString() + '\u00b0';
+                site.li.querySelector('div.fcclink a').href = 'https://transition.fcc.gov/fcc-bin/fmq?fileno=&state=&city=&freq=0.0&fre2=107.9&serv=&status=&facid=&asrn=&class=&list=0&NextTab=Results+to+Next+Page%2FTab&dist=&dlat2=&mlat2=&slat2=&NS=N&dlon2=&mlon2=&slon2=&EW=W&size=9&call=' + site.callsign;
 
                 if (site.parentCallsign == null) {
                     site.li.querySelector('div.callsign').innerText = site.callsign;
                 } else {
                     site.li.querySelector('div.callsign').innerText = site.parentCallsign + ' (via ' + site.callsign + ')';
                 }
+
+                site.li.addEventListener('click', function(event) {
+                    sites.forEach(function(site) {
+                        if (site.li === event.target) {
+                            site.li.classList.toggle('expanded');
+                        } else {
+                            site.li.classList.remove('expanded');
+                        }
+                    });
+                });
             });
 
             let update = function(position) {
@@ -98,12 +126,14 @@ fetch('sites.json').then(function(response) {
                     if (site.distance < 188) {
                         site.distance = milesFromSite(position.coords, site);
                         if (site.distance < 188) {
-                            site.bearing = bearingFromSite(position.coords, site);
-                            site.range = site.contour[site.bearing - 1];
+                            site.bearingFrom = bearingFromSite(position.coords, site);
+                            site.bearingTo = bearingToSite(position.coords, site);
+                            site.range = site.contour[site.bearingFrom - 1];
                             site.score = site.distance / site.range;
 
                             site.li.querySelector('div.distance').innerText = roundTwo(site.distance).toString() + ' mi';
-                            site.li.querySelector('div.bearing').innerText = site.bearing.toString() + '\u00b0';
+                            site.li.querySelector('div.bearing').innerText = site.bearingTo.toString() + '\u00b0';
+                            site.li.querySelector('div.range').innerText = site.range.toString() + ' mi';
                             setColor(site);
                         }
                     }
